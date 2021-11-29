78fa9828453301d632ed8d43e2c1672f6a9816c9
jullipap <jullipap@gmail.com>
2021-11-29 22:50:03 +0100

    Teacher and TeacherList

---
+++ Teacher/src/Teacher.java
@@ -0,0 +1,32 @@
+public class Teacher
+{
+  private String initials;
+
+  public Teacher(String initials)
+  {
+    this.initials=initials;
+  }
+
+  public void setInitials(String initials)
+  {
+    this.initials = initials;
+  }
+
+  public String getInitials()
+  {
+    return initials;
+  }
+
+  public boolean equals(Object obj){
+    if(!(obj instanceof Teacher)){
+      return false;
+    }
+    Teacher other=(Teacher) obj;
+
+    return initials.equals(other.initials);
+  }
+
+  public String toString(){
+    return initials;
+  }
+}
