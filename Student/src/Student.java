543da3d307c4133a9808455b50c08ae0e93cc7ac
Alex Cimpan <36129808+pheonixaxel@users.noreply.github.com>
2021-11-30 14:56:06 +0100

    ss

--- Student/src/Student.java
+++ Student/src/Student.java
@@ -1,6 +1,6 @@
 import java.util.Objects;
 
-public class Student { //vthc
+public class Student {
 
     private String name;
     private int studentNumber;
@@ -9,21 +9,20 @@ public class Student { //vthc
         this.name = name;
         this.studentNumber = studentNumber;
     }
-// SETTERS
+
     public void setName(String name) {
         this.name = name;
     }
     public void setStudentNumber(int studentNumber) {
         this.studentNumber = studentNumber;
     }
-// GETTERS
+
     public String getName() {
         return name;
     }
     public int getStudentNumber() {
         return studentNumber;
     }
-// I DO NOT KNOW HOW TO NAME THESE... i don't even know why i started to write these comments in the first place
 
 
     public boolean equals(Object o) {
