384c5cd1a16adf5e3646de4722e0e73341a7973e
Iortoo <91682413+Iortoo@users.noreply.github.com>
2021-12-03 12:56:41 +0100

    iukjfg

--- Student/src/Date.java
+++ Student/src/Date.java
@@ -1,97 +1,136 @@
-public class Date {
+public class Date
+{
 
-    private int day;
-    private int month;
-    private int year;
+  private int day;
+  private int month;
+  private int year;
 
-    public Date(int day, int month, int year)
+  public Date(int day, int month, int year)
+  {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+
+  // Copy constructor
+  public Date(Date object)
+  {
+    day = object.day;
+    month = object.month;
+    year = object.year;
+  }
+
+  public void setDay(int zi)
+  {
+    if (zi < 1)
+      throw new IllegalDateException();
+    switch (month)
     {
-        this.day = day;
-        this.month = month;
-        this.year = year;
+      case 1, 3, 5, 7, 8, 10, 12:
+      {
+        if (zi > 31)
+          throw new IllegalDateException();
+        break;
+      }
+      case 2:
+      {
+        if (isLeapYear() && zi > 29)
+          throw new IllegalDateException();
+        if (!isLeapYear() && zi > 28)
+          throw new IllegalDateException();
+        break;
+      }
+      default:
+      {
+        if (zi > 30)
+          throw new IllegalDateException();
+      }
     }
-    // Copy constructor
-    public Date(Date object)
-    {
-        day = object.day;
-        month = object.month;
-        year = object.year;
-    }
+    day = zi;
+  }
 
-    public void setDay(int day) {
-        if  (day < 1 || day > 31)
-            throw new IllegalDateException();
-        this.day = day;
-    }
-    public void setMonth(int month) {
-        if  (month < 1 || month > 12)
-            throw new IllegalDateException();
-        this.month = month;
-    }
-    public void setYear(int year) {
-        if (year < 0)
-            throw new IllegalDateException();
-        this.year = year;
-    }
+  public void setMonth(int luna)
+  {
+    if (luna > 12 || luna < 1)
+      throw new IllegalDateException();
+    month = luna;
+  }
 
-    public int getDay() {
-        return day;
-    }
-    public int getMonth() {
-        return month;
-    }
-    public int getYear() {
-        return year;
-    }
-    /*public boolean equals(MyDate object2)
+  public void setYear(int an)
+  {
+    year = an;
+  }
+
+  public boolean isLeapYear()
+  {
+    return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+  }
+
+  public int getDay()
+  {
+    return day;
+  }
+
+  public int getMonth()
+  {
+    return month;
+  }
+
+  public int getYear()
+  {
+    return year;
+  }
+
+  /*public boolean equals(MyDate object2)
+  {
+      if (day == object2.day && month == object2.month && year == object2.year)
+          return true;
+      else
+          return false;
+  }*/
+  public int compare(Object obj)
+  {
+    if (!(obj instanceof Date))
     {
-        if (day == object2.day && month == object2.month && year == object2.year)
-            return true;
+      return -2;
+    }// -2 = different object
+
+    Date other = (Date) obj;
+    // -1 = before   e      0 = same        1 = after
+    if (year > other.year)
+    {
+      return 1;
+    }
+    else if (year < other.year)
+    {
+      return -1;
+    }
+    else
+    {
+      if (month < other.month)
+        return -1;
+      else if (month > other.month)
+        return 1;
+      else
+      {
+        if (day < other.month)
+          return -1;
+        else if (day > other.day)
+          return 1;
         else
-            return false;
-    }*/
-    public int compare(Object obj)
-    {
-        if(!(obj instanceof Date))
-        {
-            return -2;
-        }// -2 = different object
-
-        Date other = (Date) obj;
-        // -1 = before   e      0 = same        1 = after
-        if (year > other.year)
-        {
-            return 1;
-        }
-        else if (year < other.year)
-        {
-            return -1;
-        }
-        else
-        {
-            if (month < other.month)
-                return -1;
-            else if (month > other.month)
-                return 1;
-            else
-            {
-                if (day < other.month)
-                    return -1;
-                else if (day > other.day)
-                    return 1;
-                else
-                    return 0;
-            }
-        }
+          return 0;
+      }
     }
+  }
 
-    // Copy method
-    public Date copy()
-    {
-        return new Date(day, month, year);
-    }
+  // Copy method
+  public Date copy()
+  {
+    return new Date(day, month, year);
+  }
 
-    public String toString() {
-        return getDay() + "/" + getMonth() + "/" + getYear();
-    }
+  public String toString()
+  {
+    return getDay() + "/" + getMonth() + "/" + getYear();
+  }
 }
