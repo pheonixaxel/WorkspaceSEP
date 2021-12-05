80e8b33789ecdf184fdff1da991ab008447df04b
Alex Cimpan <36129808+pheonixaxel@users.noreply.github.com>
2021-12-05 19:40:15 +0100

    ss

--- Student/src/Schedule.java
+++ Student/src/Schedule.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 
 public class Schedule
 {
+  @FXML private ChoiceBox semesterChoiceSchedule;
   private ArrayList<Lesson> lessons;
 
   public Schedule()
---
+++ Student/src/Test.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+import view.StartGUI;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Application.launch(StartGUI.class);
+    }
+}
